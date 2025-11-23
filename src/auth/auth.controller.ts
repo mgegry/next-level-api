56fb7caf41317f6554ec8417709ca64eef75af38
Mircea Egry <mirceageluegry@gmail.com>
2025-11-24 00:37:10 +0200

    Added CurrentUser decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,8 @@ import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 import { LocalGuard } from './guards/local.guard';
 import { LoginResponseDto } from './dtos/login-response.dto';
 import { RefreshResponseDto } from './dtos/refresh-response.dto';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from 'src/user/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -12,20 +14,20 @@ export class AuthController {
 
   @Post('login')
   @UseGuards(LocalGuard)
-  async login(@Request() req): Promise<LoginResponseDto> {
-    return this.authService.login(req.user);
+  async login(@CurrentUser() user: User): Promise<LoginResponseDto> {
+    return this.authService.login(user);
   }
 
   @Post('refresh')
   @UseGuards(JwtRefreshGuard)
-  async refreshTokens(@Request() req): Promise<RefreshResponseDto> {
-    return this.authService.refreshTokens(req.user);
+  async refreshTokens(@CurrentUser() user: User): Promise<RefreshResponseDto> {
+    return this.authService.refreshTokens(user);
   }
 
   @Post('logout')
   @UseGuards(JwtGuard)
   @HttpCode(204)
-  async logout(@Request() req): Promise<void> {
-    await this.authService.logout(req.user.id);
+  async logout(@CurrentUser() user: User): Promise<void> {
+    await this.authService.logout(user.id);
   }
 }
