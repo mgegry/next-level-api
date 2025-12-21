9af3af49ccf0a3e4fe1d6b6ab0c46ee1711d1959
Mircea Egry <mirceageluegry@gmail.com>
2025-12-21 23:03:06 +0200

    made typeorm config get variables from environment

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,33 +1,24 @@
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
-import path from 'path';
 
 import { config } from 'dotenv';
 
 const envFile =
-  process.env.NODE_ENV === 'production' ? 'config/.env.prod' : 'config/.env';
+  process.env.NODE_ENV === 'production'
+    ? 'config/.env.production'
+    : 'config/.env.development';
 
-// 👇 Absolute path from project root
-config({
-  path: path.resolve(process.cwd(), envFile),
-});
+config({ path: envFile });
 
 const configService = new ConfigService();
 
-// TODO: GET INFORMATION FROM ENVIRONMENT VARIABLES
-
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'db_erphub',
-  // host: configService.get<string>('DATABASE_HOST'),
-  // port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 5432),
-  // username: configService.get<string>('DB_USER'),
-  // password: configService.get<string>('DB_PASS')?.toString(),
-  // database: configService.get<string>('DB_NAME'),
+  host: configService.get<string>('DATABASE_HOST'),
+  port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 5432),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASS')?.toString(),
+  database: configService.get<string>('DB_NAME'),
   synchronize: false,
   entities: ['**/*.entity.ts'],
   migrations: ['src/database/migrations/*-migration.ts'],
