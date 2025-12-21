70e45a95d801cda4f65ccf4ca57f85d65be35e5f
Mircea Egry <mirceageluegry@gmail.com>
2025-12-22 01:07:14 +0200

    Changed typeorm config to read migrations also from dist

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -21,7 +21,7 @@ const AppDataSource = new DataSource({
   database: configService.get<string>('DB_NAME'),
   synchronize: false,
   entities: ['**/*.entity.ts'],
-  migrations: ['src/database/migrations/*-migration.ts'],
+  migrations: [__dirname + '/database/migration/**/*{.js,.ts}'],
   migrationsRun: false,
   logging: true,
 });
